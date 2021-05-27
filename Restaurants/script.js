8881e6248273197b73447bfce3fdc82b9f45da4f
mrosa_san <mrosa.san.mora@gmail.com>
2021-05-27 13:09:20 +0200

    select dinamico

--- Restaurants/script.js
+++ Restaurants/script.js
@@ -32,8 +32,81 @@ function onMapLoad() {
 //filter kind of food
 $('#kind_food_selector').on('change', function() {
   console.log(this.value);
+  let kind = this.value;
   render_to_map(data_markers, this.value);
+  kind_food (kind);
 });
+
+
+function kind_food (kind) {
+	let exist = document.getElementById('kind_food');
+	if(exist){
+				
+		let father = exist.parentNode;
+		father.removeChild(exist);
+		
+	}
+	
+
+	
+		//create new select options
+		let selectRestaurant = document.getElementById('restaurants');
+		//let divRest = document.getElementById('rest')
+		//let parr = document.createElement('p');
+		let element = document.createElement('select');
+		
+		element.id = ('kind_food');
+		element.name = ('select2');
+		//parr.textContent = ('Restaurantes: ')
+		
+		//selectRestaurant.appendChild(parr);
+		selectRestaurant.appendChild(element);
+		//default option
+		let optionRestaurant = document.createElement('option');
+		optionRestaurant.value = (`Elige restaurante`);
+		optionRestaurant.text = (`Elige restaurante`);
+		element.appendChild(optionRestaurant);
+		
+		//create options from db	
+		for(let i in data_markers) {
+			if(data_markers[i].kind_food.includes(kind)){
+				let optionRestaurant = document.createElement('option');
+				optionRestaurant.value = (`${data_markers[i].name}`);
+				optionRestaurant.text = (`${data_markers[i].name}`);
+				element.appendChild(optionRestaurant);
+			}
+		}
+		$('#kind_food').on('change', function() {
+			console.log(this.value);
+			renderToMapSelectRestaurant (this.value);
+			
+		});
+		
+}	
+
+//put marker on map
+function renderToMapSelectRestaurant (restaurant) {
+	
+	if(markers){
+		markers.clearLayers();
+	}
+	//add marker on map
+	for(let i in data_markers){
+		if(data_markers[i].name.includes(restaurant)){
+			markers.addLayer(L.marker([data_markers[i].lat,data_markers[i].lng ], {
+				title: `${data_markers[i].name}`
+			})
+		
+				.bindPopup(`<h1>${data_markers[i].name}</h1></p> <b>dirección</b>: ${data_markers[i].adress}
+					</p><img style="height: 100px" src='${data_markers[i].photo}'/>`)
+				.openPopup());
+				
+				
+				map.addLayer(markers);
+			}
+	}
+	
+}
 
 //put markers on map
 function render_to_map(data_markers,filter){
@@ -59,3 +132,5 @@ function render_to_map(data_markers,filter){
 			}
 		}
 }
+
+
